3f0ae2179e2a6f04ac3af826151650062dd244e3
bernadettekelly <bernadettemkelly54@gmail.com>
2017-04-13 21:02:20 -0400

    cleanup

--- js/components/app.js
+++ js/components/app.js
@@ -30,12 +30,7 @@ export default function App(props) {
 					<Route path="search" component={ContentWrapper} />
 				</Router>
 			</Provider>
-			{/*<Content 
-			title = "New York Times Search" 
-			subtitle="Search Beginning September 18th, 1851"/>
-			<LandingPage 
-			LPtitle = "New York Times Search" 
-			Explanation="New York Times Search allows users to input a start and end date (between September 18th, 1851 and the present) and receive a link, snippet, and lead of articles from that chosen period. Its main purpose is to serve as a browsing tool to stumble upon topics of a particular era."/>*/}
+			
 		</main>
 	)
 }
