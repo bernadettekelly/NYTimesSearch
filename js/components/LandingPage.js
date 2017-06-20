ef23718341d6430b541a862bacb72fd87cc92bb9
bernadettekelly <bernadettemkelly54@gmail.com>
2017-06-20 14:14:21 -0400

    changed color

--- js/components/LandingPage.js
+++ js/components/LandingPage.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router';
 
 export default function LandingPage(props) {
   return (
-    <section className="hero is-success is-fullheight">
+    <section className="hero is-light is-fullheight">
         <div className="hero-body">
           <div className="container has-text-centered">
             <h1>{props.LPTitle}</h1>
